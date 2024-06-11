93ec9b98da2101b5ccee07eee3f3b3cda4aeaefd
coolcoding101 <124744142+coolcoding101@users.noreply.github.com>
2024-06-11 03:12:48 +0000

    changed script for loading seats and using js to determine seat positions

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const scene = document.querySelector('#scene');
 const seat = document.querySelector('#seatOne');
 
 window.addEventListener('DOMContentLoaded', () => {
-    for(i = 1; i < 30; i++){
+    for(i = 1; i < 8; i++){
         const newSeat = seat.cloneNode(true);
         newSeat.setAttribute('id', 'seat' + (i + 1));
+        
         scene.appendChild(newSeat);
         console.log(newSeat);
     }
+
+    var seatTwo = document.getElementById('seat2');
+    seatTwo.setAttribute('position', '-3.257 0.064 6.347');
 });
